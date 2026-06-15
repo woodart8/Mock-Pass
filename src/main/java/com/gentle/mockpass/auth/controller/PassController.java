88052cd8bbeeab2120457a930f7959cd6f47a822
woodart8 <kibeom1145@gmail.com>
2026-06-15 15:55:34 +0900

    Refactor: remove unused import

--- src/main/java/com/gentle/mockpass/auth/controller/PassController.java
+++ src/main/java/com/gentle/mockpass/auth/controller/PassController.java
@@ -5,7 +5,6 @@ import com.gentle.mockpass.auth.dto.PassResponseDto;
 import com.gentle.mockpass.auth.service.PassService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
